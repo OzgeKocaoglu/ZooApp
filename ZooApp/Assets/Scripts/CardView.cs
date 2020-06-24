b7f0b4d9faf1ac04927e93030820115b8b98eae4
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-06-24 04:17:12 +0300

    Scriptable Objects
    Finished objects scripts.

---
+++ ZooApp/Assets/Scripts/CardView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CardView : MonoBehaviour
+{
+    public Text title;
+    public Text description;
+    public Text exhibit;
+    public Image animalImg;
+
+    public CardModel[] cards;
+
+    public void DisplayCard(int card)
+    {
+        title.text = cards[card].title;
+        description.text = cards[card].description;
+        exhibit.text = cards[card].exhibit;
+        animalImg.sprite = cards[card].animalImg;
+    }
+}
---
+++ ZooApp/Assets/Scripts/ScriptableObjects/CardModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName ="New Card", menuName = "Cards")]
+public class CardModel : ScriptableObject
+{
+    public string title;
+    public string description;
+    public string exhibit;
+    public Sprite animalImg;
+
+}
