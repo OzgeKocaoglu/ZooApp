d5f4840b7da4306d5f5b10e47d54774f6fbe3778
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-06-24 06:46:16 +0300

    Final Initial

---
+++ ZooApp/Assets/Scripts/ExitButtonUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonUI : MonoBehaviour
+{
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+}
